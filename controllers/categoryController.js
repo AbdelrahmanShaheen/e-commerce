43709f836ce874c662e0f9f8401d0f0534e94cee
Abdelrahman Shaheen <shaheenabdelrahman28@gmail.com>
2023-10-15 20:26:02 +0300

    get-categories router (using limit&page&sortBy)

--- controllers/categoryController.js
+++ controllers/categoryController.js
@@ -13,4 +13,30 @@ const createCategory = async (req, res) => {
     res.status(400).send({ error: err.message });
   }
 };
-module.exports = { getCategory, createCategory };
+const getCategories = async (req, res) => {
+  try {
+    const options = {};
+    if (req.query.page && req.query.limit) {
+      const page = parseInt(req.query.page);
+      const limit = parseInt(req.query.limit);
+      const skip = (page - 1) * limit;
+      options.skip = skip;
+      options.limit = limit;
+    }
+    if (req.query.sortBy) {
+      const parts = req.query.sortBy.split(":");
+      options.sort = {
+        [parts[0]]: parts[1] === "desc" ? -1 : 1,
+      };
+    }
+    const categories = await Category.find({}, null, options);
+    if (categories.length === 0) {
+      return res.status(404).send({ error: "No categories found" });
+    }
+    res.status(200).send({ results: categories.length, data: categories });
+  } catch (err) {
+    res.status(500).send({ error: err.message });
+  }
+};
+
+module.exports = { getCategory, createCategory, getCategories };
--- routers/categoryRouter.js
+++ routers/categoryRouter.js
@@ -2,8 +2,9 @@ const express = require("express");
 const {
   getCategory,
   createCategory,
+  getCategories,
 } = require("../controllers/categoryController.js");
 const app = express();
 const categoryRouter = express.Router();
-categoryRouter.route("/").post(createCategory).get(getCategory);
+categoryRouter.route("/").post(createCategory).get(getCategories);
 module.exports = categoryRouter;

ba1c119d4ef8e65b80a852f25e4ae560ca42b291
Abdelrahman Shaheen <shaheenabdelrahman28@gmail.com>
2023-10-16 17:24:31 +0300

    Catching Unhandled Routes exceptions & Use Global Error Handling Middleware & Catching unhandeled errors related to DB and handle it in the global handler & Adding support for prod and dev for errors

--- server.js
+++ server.js
@@ -5,6 +5,8 @@ const morgan = require("morgan");
 const categoryRouter = require("./routers/categoryRouter.js");
 const Category = require("./models/category.js");
 const { getCategory } = require("./controllers/categoryController.js");
+const AppError = require("./utils/AppError.js");
+const globalErrorHandler = require("./middlewares/errorMiddleware.js");
 const express = require("express");
 const app = express();
 
@@ -14,6 +16,12 @@ if (process.env.NODE_ENV === "development") {
   app.use(morgan("dev"));
 }
 app.use("/api/v1/categories", categoryRouter);
+//Catching Unhandled Routes
+app.all("*", (req, res, next) => {
+  next(new AppError(`cannot find ${req.originalUrl} on the server`, 404));
+});
+//Use Global Error Handling Middleware
+app.use(globalErrorHandler);
 //........................
 const port = process.env.PORT || 3000;
 app.listen(port, () => {

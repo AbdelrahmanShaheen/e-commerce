f46cda55128286de77207e10e8dcd3943f4421be
Abdelrahman Shaheen <shaheenabdelrahman28@gmail.com>
2023-10-26 01:28:03 +0300

    add describtion to signup and login

--- controllers/authController.js
+++ controllers/authController.js
@@ -4,7 +4,9 @@ const slugify = require("slugify");
 
 const AppError = require("../utils/AppError");
 const User = require("../models/user");
-
+//@desc signup
+//@route Post /api/v1/auth/signup
+//@access Public
 const signup = asyncHandler(async (req, res, next) => {
   const { name, password, email } = req.body;
   const user = await User.create({
@@ -16,6 +18,9 @@ const signup = asyncHandler(async (req, res, next) => {
   const token = user.generateAuthToken();
   res.status(201).send({ data: user, token });
 });
+//@desc login
+//@route Post /api/v1/auth/login
+//@access Public
 const login = asyncHandler(async (req, res, next) => {
   const { email, password } = req.body;
   const user = await User.findOne({ email });
@@ -25,4 +30,5 @@ const login = asyncHandler(async (req, res, next) => {
   const token = user.generateAuthToken();
   res.status(200).send({ data: user, token });
 });
+
 module.exports = { signup, login };

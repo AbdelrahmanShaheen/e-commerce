5215363110ce8454c6875e9bfe3529f174c36d35
Abdelrahman Shaheen <shaheenabdelrahman28@gmail.com>
2023-10-26 02:27:41 +0300

    Create middleware to authorize(user permissions) users

---
+++ middlewares/allowedToMiddleware.js
@@ -0,0 +1,15 @@
+const asyncHandler = require("express-async-handler");
+const AppError = require("../utils/AppError");
+
+const allowedTo = (...roles) =>
+  asyncHandler(async (req, res, next) => {
+    const { user } = req;
+    if (!roles.includes(user.role)) {
+      return next(
+        new AppError("You are not allowed to access this route", 403)
+      );
+    }
+    next();
+  });
+
+module.exports = allowedTo;

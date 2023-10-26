92c014db7bd75ed707988b6b6f2289f3827d0b44
Abdelrahman Shaheen <shaheenabdelrahman28@gmail.com>
2023-10-26 16:32:29 +0300

    implement send email logic

---
+++ utils/sendEmail.js
@@ -0,0 +1,23 @@
+const nodemailer = require("nodemailer");
+
+const sendEmail = async (options) => {
+  const transporter = nodemailer.createTransport({
+    service: "gmail",
+    auth: {
+      user: process.env.EMAIL_USER,
+      pass: process.env.EMAIL_PASSWORD,
+    },
+  });
+
+  const mailOpts = {
+    from: "E-shop App <shaheenabdelrahman28@gmail.com>",
+    to: options.email,
+    subject: options.subject,
+    text: options.message,
+  };
+
+  // 3) Send email
+  await transporter.sendMail(mailOpts);
+};
+
+module.exports = sendEmail;

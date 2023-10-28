e3f4f8378e9cd003be0b0ed3df3e3e5d0b55774b
Abdelrahman Shaheen <shaheenabdelrahman28@gmail.com>
2023-10-28 12:49:13 +0200

    Review Schema & Model

---
+++ models/review.js
@@ -0,0 +1,27 @@
+const mongoose = require("mongoose");
+const reviewSchema = new mongoose.Schema(
+  {
+    title: {
+      type: String,
+    },
+    ratings: {
+      type: Number,
+      min: [1, "Rating must be above or equal 1.0"],
+      max: [5, "Rating must be below or equal 5.0"],
+    },
+    product: {
+      type: mongoose.Schema.ObjectId,
+      ref: "Product",
+      required: [true, "Review must belong to product"],
+    },
+    user: {
+      type: mongoose.Schema.ObjectId,
+      ref: "User",
+      required: [true, "Review must belong to user"],
+    },
+  },
+  { timestamps: true }
+);
+
+const Review = mongoose.model("Review", reviewSchema);
+module.exports = Review;
